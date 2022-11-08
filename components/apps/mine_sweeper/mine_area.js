0ade8dd18f024bf23b1f5c7ef64629ff72cd3fc1
tmxz <tmxz@qq.com>
2022-11-08 17:17:46 +0800

    开始做扫雷

--- components/apps/mine_sweeper/mine_area.js
+++ components/apps/mine_sweeper/mine_area.js
@@ -7,8 +7,8 @@ const Mine_area = () => {
     const canvasRef = useRef(null);
 
     let map = [];
-    let map_width = 8;
-    let map_height = 8;
+    let map_width = 9;
+    let map_height = 9;
     let mine_num = 10;
     class Grid{
         constructor(){
@@ -39,16 +39,75 @@ const Mine_area = () => {
         }
         coord.my_forEach((xy) => {
             map[xy[0]][xy[1]].mine = true;
-        }); 
-    },[]);
+        });
+
+        //行i纵j
+        map.my_forEach((row, i) => {
+            row.my_forEach((grid, j) => {
+                let num = 0;
+                if(!grid.mine){
+                    try{
+                        if(map[i - 1][j].mine) num++;
+                    }catch(err){ }//左
+                    try{
+                        if(map[i + 1][j].mine) num++;
+                    } catch(err){ }//右
+                    try{
+                        if (map[i - 1][j - 1].mine) num++;
+                    }catch(err){ }//左下
+                    try{
+                        if (map[i - 1][j + 1].mine) num++;
+                    }catch(err){ }//左上
+                    try{
+                        if (map[i + 1][j - 1].mine) num++;
+                    }catch(err){ }//右下
+                    try{
+                        if (map[i + 1][j + 1].mine) num++;
+                    }catch(err){ }//右上
+                    try{
+                        if (map[i][j + 1].mine) num++;
+                    }catch(err){ }//上
+                    try{
+                        if (map[i][j - 1].mine) num++;
+                    }catch(err){ }//下
+                    grid.num = num;
+                }
+                
+            });
+
+        });
+    },[]);//地图初始化
     useEffect(() => {
         const canvas_node = canvasRef.current;
         const context = canvas_node.getContext("2d");
 
-        context.fillStyle = "ff0000";
+        
         map.my_forEach((row,i) => {
             row.my_forEach((grid, j) => {
-                context.fillRect(0 + j * 25, 0 + i * 25, 20, 20);
+                if(grid.num !== 0){
+                    context.fillStyle = "#c0c0c0";
+                    context.fillRect(j * 22.2,i * 22.2, 22, 22);
+                    context.fillStyle = "#000000";
+                    context.fillText(grid.num, 7 + j * 22.2, 14 + i * 22.2);
+                }
+                else if(grid.mine){
+                    context.fillStyle = "#c0c0c0";
+                    context.fillRect(j * 22.2,i * 22.2, 22, 22);
+                    context.fillStyle = "#000000";
+                    context.fillRect(8 + j * 22.2,8 +i * 22.2, 6, 6);
+                }
+                else{
+                    context.fillStyle = "#ffffff";
+                    context.fillRect(j * 22.2, i * 22.2, 22.2, 2);
+                    context.fillRect(j * 22.2, i * 22.2, 2, 22.2);
+    
+                    context.fillStyle = "#c0c0c0";
+                    context.fillRect(2 + j * 22.2, 2 + i * 22.2, 18, 18);
+    
+                    context.fillStyle = "#808080";
+                    context.fillRect(20 + j * 22.2, i * 22.2, 2, 22.2);
+                    context.fillRect(j * 22.2, 20 + i * 22.2, 22.2, 2);   
+                }
             })
         });
 
@@ -57,7 +116,10 @@ const Mine_area = () => {
     return (
         <canvas
             className="mine_area_canvas"
-            ref={canvasRef}/>
+            ref={canvasRef}
+            width="200px"
+            height="200px"
+        />
     )
 
 }
